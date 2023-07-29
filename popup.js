0ca5f7ba199fdf724024312be2c3f77b94dfa249
Yash <129624039+its-me-yps@users.noreply.github.com>
2023-07-29 16:22:01 +0530

    Functions Added

--- popup.js
+++ popup.js
@@ -1,23 +1,21 @@
 document.getElementById('updateBtn').addEventListener('click', async function() {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.runtime.sendMessage({ action: 'GetTT', tabId: tab.id });
-  //document.getElementById('timetableBtn').classList.remove('hide');
-  window.alert('Yout Time Table has been Successfly Updated. Now, Everytime Chrome will Notify you about your upcoming class in 15 min advance.')
 });
 
 
+chrome.storage.local.get(['personal_data'], function(result) {
+  console.log(result.personal_data);
+  if(Object.keys(result).length){
+    Add_DashBoard(result.personal_data)
+  }
+})
 
 
-
-
-
-
-
-
-
-
-
-
+document.getElementById('Show_TT').addEventListener('click', async function() {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.runtime.sendMessage({ action: 'ShowTT', tabId: tab.id });
+});
 
 
 document.getElementById('upcomingClass').addEventListener('click',async function() {
@@ -36,11 +34,10 @@ document
     document.getElementsByClassName("personal")[0].style.display = "none";
     const ContainerElement = document.getElementsByClassName("container")[0];
     let closeButton = document.createElement("button");
-    ContainerElement.style.width = "760px";
+    ContainerElement.style.width = "798px";
     closeButton.innerHTML = "Back";
     closeButton.id = "closeButton";
     document.getElementById("closeBtn").appendChild(closeButton);
-    // document.getElementById("closeButton").style="color:#001c30";
 
     document.getElementById("closeButton").addEventListener("click", () => {
       const ContainerElement = document.getElementsByClassName("container")[0];
@@ -63,8 +60,7 @@ document
     deleteButton.innerHTML = "Delete Class";
     deleteButton.id = "deleteButton";
     document.getElementById("timetableBtns").appendChild(deleteButton);
-    document.getElementById('deleteButton').style="color:black";
-  
+
     let deleteButtonClicked = false;
 
     let deleteBtn = document.getElementById("deleteButton");
@@ -73,8 +69,8 @@ document
       if (deleteButtonClicked) {
         deleteButtonClicked = false;
         deleteBtn.innerHTML = "Delete Class";
-        deleteBtn.style.backgroundColor = "#64ccc5";
-        deleteBtn.style.color = "black";
+        deleteBtn.style.backgroundColor = "#b38add";
+        deleteBtn.style.color = "white";
         deleteBtn.style.border = "none";
         let minusSigns = document.getElementsByClassName("minus-btn");
 
@@ -84,9 +80,9 @@ document
       } else {
         deleteButtonClicked = true;
         deleteBtn.innerHTML = "Stop Deleting";
-        deleteBtn.style.backgroundColor = "#dafffb";
-        deleteBtn.style.color = "#176b87";
-        deleteBtn.style.border = "2px solid #176b87";
+        deleteBtn.style.backgroundColor = "white";
+        deleteBtn.style.color = "#4c7bfa";
+        deleteBtn.style.border = "2px solid #4c7bfa";
         let minusSigns = document.getElementsByClassName("minus-btn");
 
         Array.from(minusSigns).forEach(function (minusSign) {
@@ -204,7 +200,7 @@ function addTT() {
                            <div class="title">${title}</div>
                            <div class="${title} txt-btn"></div>
                            <div class="add-btn-container">
-                           <input type="text" class="${title}Inp" placeholder="Add LHC">
+                           <input type="text" class="${title}Inp" placeholder="Enter Lecture Hall">
                            <button class="${title}Btn add-btn">+</button>
                            </div>
                           </div>`;
@@ -217,102 +213,102 @@ function addTT() {
   });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function Add_DashBoard(x){
+  let personalDataDiv=document.createElement('div');
+  personalDataDiv.classList.add("personalData");
+  let imgDiv=document.createElement('div');
+  let imgDivBox=document.createElement('div');
+  imgDiv.classList.add("imgDiv");
+  imgDivBox.classList.add("imgDivBox");
+  let img=document.createElement('img');
+  let nameDiv=document.createElement('div');
+  let listDiv=document.createElement('div');
+  nameDiv.classList.add('nameDiv');
+  listDiv.classList.add('listDiv');
+  let n=document.createElement('h1');
+  let r=document.createElement('h3');
+  let list=document.createElement('ul');
+  list.classList.add('list');
+  n.innerHTML="<b>"+x.name+"</b>";
+  r.innerHTML="<b>"+x.roll_no+"</b>";
+  nameDiv.appendChild(n);
+  nameDiv.appendChild(r);
+  personalDataDiv.appendChild(nameDiv);
+  let val=x.dp;
+  img.src=val;
+  imgDiv.appendChild(imgDivBox);
+  imgDiv.appendChild(img);
+  document.getElementsByClassName('personal')[0].appendChild(imgDiv);
+  let programme=document.createElement('li');
+  programme.innerHTML="Programme : "+"<b>"+x.programme+"</b>";
+  list.appendChild(programme);
+  let dept=document.createElement('li');
+  dept.innerHTML="Department : " + "<b>"+x.dept+"</b>";
+  list.appendChild(dept);
+  let sem=document.createElement('li');
+  sem.innerHTML="Semester : " + "<b>"+x.sem+"</b>";
+  list.appendChild(sem);
+  let appliedCredits=document.createElement('li');
+  appliedCredits.innerHTML="Applied Credits: "+"<b>"+x.appliedCredits+"</b>";
+  list.appendChild(appliedCredits);
+  listDiv.appendChild(list);
+  personalDataDiv.appendChild(listDiv);
+  document.getElementsByClassName('personal')[0].appendChild(personalDataDiv);
+  document.getElementsByTagName('img')[0].style="border-radius:100%;width:110px;height:110px;object-fit:cover;object-position:0% 0%;margin-top:auto;margin-bottom:auto;";
+}
+
+
+function LabClashes(){
+  chrome.storage.local.get(['timetable'], function(result) {
+    const timetable = result.timetable;
+    for(let j=0; j<5; j++){
+      const x = timetable[Day(j)]
+      for(let i=0; i<x.length-1; i++){
+        if(x[i].time == x[i+1].time){
+          console.log(x[i]);
+          class_cell = Array.from(document.getElementsByClassName("timetable-day"))[j].getElementsByClassName("class-cell")
+          Array.from(class_cell).forEach((elem) => {
+            if(elem.getElementsByClassName("time")[0].innerText == x[i].time){
+              if(elem.classList.contains('blue-border')){
+                elem.classList.remove('blue-border');
+                elem.classList.add('red-border');
+              }
+              else{
+                elem.classList.remove('red-border');
+                elem.classList.add('blue-border');
+              }
+              elem.style.border = '1px solid red';
+            }
+          });
+        }
+      }
+    }
+  });
+}
+
+document.getElementById("LHC").addEventListener("click", async function() {
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  chrome.runtime.sendMessage({ action: "LHC", tabId: tab.id});
+});
+
+
+function LHC(){
+  chrome.storage.local.get(['LHC'], function(result) {
+    const LHC = result.LHC
+    if(LHC == null){
+      return
+    }
+    for(let course in LHC){
+      const title = document.getElementsByClassName('Lec-'+course+'Inp');
+      const titleArray = Array.from(title);
+      titleArray.forEach((element) => {
+        element.value = LHC[course]
+        const addButton = document.querySelector('.Lec-' + course + 'Btn');
+        addButton.click();
+      });
+    }
+  });
+}
 
 function Day(d){
   if(d == 0){
